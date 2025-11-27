bd01ca03dcdcf9f74212790dee39edfb1bf80e4e
shwetha-2006 <sujatasujata45539@gmail.com>
2025-11-27 09:56:01 +0530

    product.py

--- product.py
+++ product.py
@@ -1,12 +1,23 @@
+import sys
 def get_product_info(product_id, name, quantity, price):
-    """
-    Return a formatted string containing product details.
-    """
     return (
         f"Product ID:{product_id}\n"
         f"Product Name:{name}\n"
         f"Quantity:{quantity}\n"
         f"Price:{price}"
     )
+
 if __name__ == "__main__":
-    print(get_product_info("P001", "Laptop", 5, 1200.50))
+  
+    if len(sys.argv) < 5:
+        print("product details\n")
+        product_id = "P001"
+        name = "Laptop"
+        quantity = "5"
+        price = "1200.50"
+    else:
+        product_id = sys.argv[1]
+        name = sys.argv[2]
+        quantity = sys.argv[3]
+        price = sys.argv[4]
+print(get_product_info(product_id, name, quantity, price))
